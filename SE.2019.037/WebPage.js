45ad3522a6fd1d25977558f166a0585bf3c2de1e
VijithaBandara <94214061+VijithaBandara@users.noreply.github.com>
2021-11-14 09:55:46 +0530

    web page was added

---
+++ SE.2019.037/WebPage.js
@@ -0,0 +1,10 @@
+    function myfun(){
+        alert("Vegetables");}
+    function myfun1(){
+        alert("Fresh Friuts");}
+    function myfun2(){
+        alert("Dried Friuts");}
+     function myfun3(){
+        alert("Vegetables");}
+    function myfun4(){
+        alert("Drink Friuts");}
